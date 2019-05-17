c98ccc65f2ae196abae772d0f3d340b492118849
angel prats <angelpratz@gmail.com>
2019-05-17 13:14:31 -0400

    working on map and  prototypes  on cards and tab js

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -4,11 +4,19 @@ import Card from './Card';
 const Cards = props => {
   return (
     <div className="cards-container">
+      {props.Cards.map(card => (
+        <Card card={card} />
+      ))}
+
+
+
+
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
   )
 }
+
 
 // Make sure you include prop types for all of your incoming props
 
