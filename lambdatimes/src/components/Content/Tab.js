df574e61c4feb1e37cfdf68d33790768a2589dc1
angel prats <angelpratz@gmail.com>
2019-05-17 18:07:19 -0400

    created if statement and fixed mispelling bug in tab js

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -2,14 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Tab = props => {
-
+  let tabItem = '';
+  if (props.tab === props.selectedTab) {
+    tabItem = 'tab active-tab';
+  } else {
+    tabItem = 'tab';
+  }
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+
   return (
     <div
-      className={''}
+      className={'tabItem'}
       onClick={() => {
+        props.selectTabHandler(props.tab)
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
@@ -18,6 +26,10 @@ const Tab = props => {
     </div>
   );
 };
+
+Tab.propTypes = {
+  tab: PropTypes.string
+}
 
 // Make sure you include PropTypes on your props.
 
