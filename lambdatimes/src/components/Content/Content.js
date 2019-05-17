de9a92e9f48808f40ee3112082b248dfa1d7e3c5
angel prats <angelpratz@gmail.com>
2019-05-17 17:51:50 -0400

    stuck on tab js, imported proptypes

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -10,19 +10,20 @@ export default class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      selected: 'all',
       tabs: [],
       cards: []
     };
   }
 
   componentDidMount() {
-    this.setState({ selected: 'all' }, { tabs: tabData }, { cards: cardData })
+    this.setState({ tabs: tabData, cards: cardData })
     // Once the component has mounted, get the data and reflect that data on the state.
   }
 
   changeSelected = tab => {
     this.setState({
-
+      selected: tab
     })
     // this function should take in the tab and update the state with the new tab.
   };
